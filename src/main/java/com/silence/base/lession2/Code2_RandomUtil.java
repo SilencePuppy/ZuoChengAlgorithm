3bb0f1e7a42316879b39f00409a2698dbc35ef4b
lixiaobing <lixiaobing@huice.com>
2022-02-18 19:19:58 +0800

    对数器

---
+++ src/main/java/com/silence/base/lession2/Code2_RandomUtil.java
@@ -0,0 +1,11 @@
+package com.silence.base.lession2;
+
+/**
+ * 对数器
+ * 如何设置 0 1 平均发生器
+ * @author lixiaobing
+ * @Classname Code2_RandomUtil
+ * @date 2022-02-18 19:18
+ */
+public class Code2_RandomUtil {
+}
